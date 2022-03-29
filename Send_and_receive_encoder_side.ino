48adca9ec2602c84e7535cb02892ac274106ad2a
Nastrawww <102368584+Nastrawww@users.noreply.github.com>
2022-03-29 15:31:54 +0530

    Add files via upload

---
+++ Send_and_receive_encoder_side.ino
@@ -0,0 +1,168 @@
+#include <RadioLib.h>
+
+// SX1278 has the following connections:
+// NSS pin:   10
+// DIO0 pin:  2
+// RESET pin: 9
+// DIO1 pin:  3
+SX1278 radio = new Module(10, 2, 9, 3);
+int CLK = 8;  // Pin 9 to clk on encoder
+int DT = 7;  // Pin 8 to DT on encoder
+int RotPosition = 0; 
+int rotation;  
+int value;
+boolean LeftRight;
+
+// or using RadioShield
+// https://github.com/jgromes/RadioShield
+//SX1278 radio = RadioShield.ModuleA;
+
+void setup() {
+  Serial.begin(9600);
+
+  // initialize SX1278 with default settings
+  Serial.print(F("[SX1278] Initializing ... "));
+  int state = radio.begin();
+  if (state == RADIOLIB_ERR_NONE) {
+    Serial.println(F("success!"));
+  } else {
+    Serial.print(F("failed, code "));
+    Serial.println(state);
+    while (true);
+  }
+
+  // some modules have an external RF switch
+  // controlled via two pins (RX enable, TX enable)
+  // to enable automatic control of the switch,
+  // call the following method
+  // RX enable:   4
+  // TX enable:   5
+  /*
+    radio.setRfSwitchPins(4, 5);
+  */
+}
+
+void loop() {
+  Serial.print(F("[SX1278] Transmitting packet ... "));
+
+  // you can transmit C-string or Arduino string up to
+  // 256 characters long
+  // NOTE: transmit() is a blocking method!
+  //       See example SX127x_Transmit_Interrupt for details
+  //       on non-blocking transmission method.
+  encoder();
+  String message = String(analogRead(RotPosition));
+  int state = radio.transmit(message);
+
+  // you can also transmit byte array up to 256 bytes long
+  /*
+    byte byteArr[] = {0x01, 0x23, 0x45, 0x67, 0x89, 0xAB, 0xCD, 0xEF};
+    int state = radio.transmit(byteArr, 8);
+  */
+
+  if (state == RADIOLIB_ERR_NONE) {
+    // the packet was successfully transmitted
+    Serial.println(F(" success!"));
+
+    // print measured data rate
+    Serial.print(F("[SX1278] Datarate:\t"));
+    Serial.print(radio.getDataRate());
+    Serial.println(F(" bps"));
+
+  } else if (state == RADIOLIB_ERR_PACKET_TOO_LONG) {
+    // the supplied packet was longer than 256 bytes
+    Serial.println(F("too long!"));
+
+  } else if (state == RADIOLIB_ERR_TX_TIMEOUT) {
+    // timeout occurred while transmitting packet
+    Serial.println(F("timeout!"));
+
+  } else {
+    // some other error occurred
+    Serial.print(F("failed, code "));
+    Serial.println(state);
+
+  }
+
+  // wait for message to receive
+  //delay(2000);
+  receivemessage();
+  delay(1000);
+}
+int encoder()
+{
+   value = digitalRead(CLK);  //ENCODER
+   if (value != rotation)
+     { // we use the DT pin to find out which way we turning.  
+        if (digitalRead(DT) != value) 
+        {  // Clockwise
+           RotPosition ++;
+           LeftRight = true;
+        }
+        else
+        { //Counterclockwise
+           LeftRight = false;
+           RotPosition--;
+        }
+        if (LeftRight)
+        { 
+           Serial.println ("clockwise");
+        }
+        else
+        {          
+           Serial.println("counterclockwise");
+        }
+     Serial.print("Encoder RotPosition: ");
+     Serial.println(RotPosition);
+   } 
+   rotation = value;
+   return RotPosition;
+   delay(10000);
+}
+void receivemessage()
+{
+  Serial.print(F("Wating for acknowlegement ............"));
+  String str;
+  int state = radio.receive(str);
+   if (state == RADIOLIB_ERR_NONE) {
+    // packet was successfully received
+    Serial.println(F("success!"));
+
+    // print the data of the packet
+    Serial.print(F("[SX1278] Data:\t\t\t"));
+    Serial.println(str);
+
+    // print the RSSI (Received Signal Strength Indicator)
+    // of the last received packet
+    Serial.print(F("[SX1278] RSSI:\t\t\t"));
+    Serial.print(radio.getRSSI());
+    Serial.println(F(" dBm"));
+
+    // print the SNR (Signal-to-Noise Ratio)
+    // of the last received packet
+    Serial.print(F("[SX1278] SNR:\t\t\t"));
+    Serial.print(radio.getSNR());
+    Serial.println(F(" dB"));
+
+    // print frequency error
+    // of the last received packet
+    Serial.print(F("[SX1278] Frequency error:\t"));
+    Serial.print(radio.getFrequencyError());
+    Serial.println(F(" Hz"));
+
+  } else if (state == RADIOLIB_ERR_RX_TIMEOUT) {
+    // timeout occurred while waiting for a packet
+    Serial.println(F("timeout!"));
+
+  } else if (state == RADIOLIB_ERR_CRC_MISMATCH) {
+    // packet was received, but is malformed
+    Serial.println(F("CRC error!"));
+
+  } else {
+    // some other error occurred
+    Serial.print(F("failed, code "));
+    Serial.println(state);
+
+  }
+}
+
